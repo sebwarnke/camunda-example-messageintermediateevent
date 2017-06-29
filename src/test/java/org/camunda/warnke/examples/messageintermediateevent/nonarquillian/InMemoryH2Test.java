206e76911100302c594007895f052d0248f58ff5
Sebastian Warnke <sebastian.warnke@camunda.com>
2017-06-29 11:23:13 +0200

    initial commit

---
+++ src/test/java/org/camunda/warnke/examples/messageintermediateevent/nonarquillian/InMemoryH2Test.java
@@ -0,0 +1,71 @@
+package org.camunda.warnke.examples.messageintermediateevent.nonarquillian;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+public class InMemoryH2Test {
+
+  @ClassRule
+  @Rule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
+
+  private static final String PROCESS_DEFINITION_KEY = "messageintermediateevent";
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  /**
+   * Just tests if the process definition is deployable.
+   */
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testParsingAndDeployment() {
+    // nothing is done here, as we just want to check for exceptions during deployment
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testMessageCorrelationA() {
+	  ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+	  
+	  assertThat(processInstance).isWaitingAt("EventBasedGateway");
+	  
+	  runtimeService().createMessageCorrelation("A").correlateWithResult();
+	  
+	  assertThat(processInstance).hasPassed("IntermediateCatchEvent_MessageA", "ScriptTask_ExecuteA");
+	  assertThat(processInstance).isEnded();
+  }
+  
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testMessageCorrelationB() {
+	  ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+	  
+	  assertThat(processInstance).isWaitingAt("EventBasedGateway");
+	  
+	  runtimeService().createMessageCorrelation("B").correlateWithResult();
+	  
+	  assertThat(processInstance).hasPassed("IntermediateCatchEvent_MessageB", "ScriptTask_ExecuteB");
+	  assertThat(processInstance).isEnded();
+  }
+
+}
